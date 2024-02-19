eba394381eac66c23f3486d4d3bf47cbdc1c96bf
Trinity <thutson3876@gmail.com>
2024-02-19 14:48:35 -0600

    Continued work on Arrangements

--- FlowerGame/Assets/Scripts/ArrangementTable.cs
+++ FlowerGame/Assets/Scripts/ArrangementTable.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ArrangementTable : MonoBehaviour
 {
+    [SerializeField]
+    InputActionAsset playerInput;
+
+    [SerializeField]
+    Camera arrangementCam;
+
     [Header("Bounds")]
     [SerializeField]
     float upperYBound = 5;
@@ -23,5 +30,25 @@ public class ArrangementTable : MonoBehaviour
     [SerializeField]
     float sensitivity;
 
+    // Flower Selected
+    GameObject selectedFlower = null;
 
+    private void Awake()
+    {
+        if (playerInput == null)
+            Debug.LogError("Player Input Null!");
+    }
+
+    // Player Input
+    public void OnSelect()
+    {
+        InputAction select = playerInput.FindAction("Select");
+        if (select == null)
+            return;
+
+        Vector2 mousePos = select.ReadValue<Vector2>();
+        Vector3 mouseWorldPos = arrangementCam.ScreenToWorldPoint(mousePos);
+
+        RaycastHit hit = Physics.Raycast();
+    }
 }
