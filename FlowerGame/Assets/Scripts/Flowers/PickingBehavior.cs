d4b8241fb9fd2e066cc95d5a7301765926744fe8
lilbbusa <bbostergames@gmail.com>
2024-02-21 14:40:53 -0600

    You can pick up

--- FlowerGame/Assets/Scripts/Flowers/PickingBehavior.cs
+++ FlowerGame/Assets/Scripts/Flowers/PickingBehavior.cs
@@ -1,18 +1,26 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PickingBehavior : MonoBehaviour
 {
+
+
     public PlayerInput PlayerInputInstance;
     public InputAction PickUp;
-    public InputAction PutDown;
 
-    [SerializeField] private Transform slot;
-    [SerializeField] private Camera characterCamera;
+    public PlayerController PC;
 
-    private PickableItem pickedItem;
+    public List<Vector3> inventory;
+    public GameObject itemToAdd;
+    public GameObject thing;
+    public Vector3 whereToPut;
+    public GameObject Player;
+    public Transform where;
 
     private void Start()
     {
@@ -22,62 +30,45 @@ public class PickingBehavior : MonoBehaviour
         PickUp = PlayerInputInstance.currentActionMap.FindAction("PickUp");
 
         PickUp.started += PickUp_started;
-        PickUp.canceled += PickUp_canceled;
+        PickUp.canceled -= Pickup_cancelled;
+
+        inventory.Add(new Vector3(0.8255777f, 0.4400001f, 1.63f));
+    }
+    private void Update()
+    {
+        whereToPut = thing.transform.position;
     }
 
-
-    private void PickUp_canceled(InputAction.CallbackContext obj)
+    private void Pickup_cancelled(InputAction.CallbackContext context)
     {
         print("cancelled");
     }
 
-    private void PickUp_started(InputAction.CallbackContext obj)
+    private void PickUp_started(InputAction.CallbackContext context)
     {
-        if(pickedItem)
+        PickItem();
+    }
+
+    private void PickItem()
+    {
+        int size = PC.flowers.Count;
+        for (int i = 0; i < size; i++)
         {
-            DropItem(pickedItem);
-        }
-        else
-        {
-            var ray = characterCamera.ViewportPointToRay(Vector3.one * 0.5f);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 1.5f))
+            if (PC.flowers[i] != null)
             {
-                var pickable = hit.transform.GetComponent<PickableItem>();
-                if (pickable)
-                {
-                    PickItem(pickable);
-                }
+                itemToAdd = PC.flowers[i];
             }
         }
-    }
-    
-    private void PickItem(PickableItem item)
-    {
-        pickedItem = item;
-        item.Rb.isKinematic = true;
-        item.Rb.velocity = Vector3.zero;
-        item.Rb.angularVelocity = Vector3.zero;
-
-        item.transform.SetParent(slot);
-
-        item.transform.position = Vector3.zero;
-        item.transform.localEulerAngles = Vector3.zero;
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            itemToAdd.transform.position = whereToPut;
+            itemToAdd.transform.parent = where;
+        }
     }
 
-    private void DropItem(PickableItem item)
-    {
-        pickedItem = null;
-
-        item.transform.SetParent(null);
-
-        item.Rb.isKinematic = false;
-
-        item.Rb.AddForce(item.transform.forward * 2, ForceMode.VelocityChange);
-    }
     private void OnDestroy()
     {
         PickUp.started -= PickUp_started;
-        PickUp.canceled -= PickUp_canceled;
+        PickUp.canceled += Pickup_cancelled;
     }
 }
