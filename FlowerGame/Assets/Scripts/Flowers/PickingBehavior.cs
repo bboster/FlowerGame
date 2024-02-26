73eb9d1f4f277e1dca268a313ec3a872be4ce7f1
lilbbusa <bbostergames@gmail.com>
2024-02-26 14:37:53 -0600

    Consolidated the flowerLocations

--- FlowerGame/Assets/Scripts/Flowers/PickingBehavior.cs
+++ FlowerGame/Assets/Scripts/Flowers/PickingBehavior.cs
@@ -21,22 +21,7 @@ public class PickingBehavior : MonoBehaviour
     // The item to be added to the bouquet, get's updated dynamically
     public GameObject itemToAdd;
 
-    // These are transforms that can be found under the main camera. Must be dragged into inspector.
-    public Transform where;
-    public Transform where2;
-    public Transform where3;
-    public Transform where4;
-    public Transform where5;
-    public Transform where6;
-    public Transform where7;
-    public Transform where8;
-    public Transform where9;
-    public Transform where10;
-    public Transform where11;
-    public Transform where12;
-    public Transform where13;
-    public Transform where14;
-    public Transform where15;
+    [SerializeField] List<Transform> flowerLocations;
 
     [Space]
     [Header("Bouqet")]
@@ -59,22 +44,6 @@ public class PickingBehavior : MonoBehaviour
         PickUp.started += PickUp_started;
         PickUp.canceled -= Pickup_cancelled;
 
-        // Adds all of the transforms that are found under the main camera.
-        flowers.Add(where);
-        flowers.Add(where2);
-        flowers.Add(where3);
-        flowers.Add(where4);
-        flowers.Add(where5);
-        flowers.Add(where6);
-        flowers.Add(where7);
-        flowers.Add(where8);
-        flowers.Add(where9);
-        flowers.Add(where10);
-        flowers.Add(where11);
-        flowers.Add(where12);
-        flowers.Add(where13);
-        flowers.Add(where14);
-        flowers.Add(where15);
     }
 
     private void Pickup_cancelled(InputAction.CallbackContext context)
@@ -122,8 +91,8 @@ public class PickingBehavior : MonoBehaviour
             {
                 // Set the item to add, transform it's position to the transform position of the values under main camera, and set the value under the main camera as the flower's parent. 
                 itemToAdd = newItem;
-                itemToAdd.transform.position = flowers[i].position;
-                itemToAdd.transform.parent = flowers[i];
+                itemToAdd.transform.position = flowerLocations[i].position;
+                itemToAdd.transform.parent = flowerLocations[i];
             }
         }
     }
