c61212306e036fe6fdbeb820172d84e5388b3870
Trinity <thutson3876@gmail.com>
2024-02-29 17:41:22 -0600

    Added SymbolHolder for UI

---
+++ FlowerGame/Assets/Scripts/UI/Customer/SymbolHolder.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SymbolHolder : MonoBehaviour
+{
+    [SerializeField]
+    GameObject symbolPrefab;
+
+    [SerializeField]
+    float sizeScalar = 0.1f;
+
+    public void GenerateSymbols(List<FlowerStatContainer> flowerStatContainers)
+    {
+        foreach(FlowerStatContainer container in flowerStatContainers)
+        {
+            GameObject obj = Instantiate(symbolPrefab, transform);
+            Image image = obj.GetComponent<Image>();
+
+            image.sprite = container.stat.symbol;
+            obj.transform.localScale *= Mathf.Clamp01(container.statAmount * sizeScalar);
+        }
+    }
+}
